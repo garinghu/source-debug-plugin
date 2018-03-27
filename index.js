98ee235ce381868319fb7116797f7b67fb55a6aa
garinghu <179841052@qq.com>
2018-03-27 15:32:56 +0800

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var path = require('path')
+var sheller = require('./lib/shell')
+
+function SourceDebugPlugin (options) {
+  this.folderName = options[0]
+  this.options = options[1]
+  this.shell = new sheller(this.folderName)
+}
+
+SourceDebugPlugin.prototype.apply = function (compiler) {
+  
+  this.shell.shellMkdir()
+  for(var key in compiler.options.resolve.alias){
+      for(var urlKey in this.options){
+          if(key == urlKey){
+              this.shell.shellClone(this.options[urlKey])
+              compiler.options.resolve.alias[key] = path.resolve(__dirname, '../../' + this.folderName + '\\' + getGitFolderName(this.options[urlKey]))
+          }
+      }
+  }
+
+}
+
+
+function getGitFolderName (url) {
+    var s = url.split('/')
+    return s[s.length - 1].match(/(\S*)(\.git)$/)[1]
+}
+
+module.exports = SourceDebugPlugin
